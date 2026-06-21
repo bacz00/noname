bfa098dc5792d4f1803d6f4f387db8fe192ec5df
bacz <57490121+bacz00@users.noreply.github.com>
2026-06-21 23:34:38 +0800

    添加阵营

--- packages/extension/starlight/src/index.ts
+++ packages/extension/starlight/src/index.ts
@@ -7,7 +7,44 @@ export default function (): importExtensionConfig {
 		name: "starlight",
 		editable: false,
 		connect: true,
-		content: function () {},
+		content: function (config, pack) {
+			const groups = {
+				seisho: "圣翔",
+				rinmeikan: "凛明馆",
+				frontier: "芙罗提亚",
+				seekfelt: "西格菲尔特",
+				seiran: "青岚",
+			};
+			game.dynamicStyle.addObject({
+				"div.starlight-group-icon": {
+					display: "none",
+				}, // 默认不显示图标
+				"div.button.character.newstyle > div.identity > div > span.starlight-group-translation": {
+					display: "none",
+				}, // 当位于角色div中时，隐藏翻译
+				"div.button.character.newstyle > div.identity > div:has(div.starlight-group-icon)": {
+					width: "16px",
+					height: "16px",
+				}, // 当位于角色div中时，设置外层div尺寸
+				"div.button.character.newstyle > div.identity > div > div.starlight-group-icon": {
+					display: "inherit",
+					width: "16px",
+					height: "16px",
+				}, // 当位于角色div中时，显示图标并设置尺寸
+				...Object.fromEntries(
+					Object.keys(groups).map(group => [
+						`div.button.character.newstyle > div.identity > div > div.starlight-group-icon.starlight-${group}`,
+						{ background: `url(extension/starlight/image/group/group_${group}.png) no-repeat center / contain` },
+					])
+				),
+			});
+
+			Object.entries(groups).forEach(([group, translation]) => {
+				lib.group.add(group);
+				lib.translate[group] =
+					`<span class="starlight-group-translation">${translation}</span><div class="starlight-${group} starlight-group-icon"></div>`;
+			});
+		},
 		precontent: function () {},
 		config: {},
 		help: {},
